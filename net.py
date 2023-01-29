ff8f3e8e202311a3ef87a8a76a0671f68ef2e0c9
maxicraftone <trashmco@gmail.com>
2023-01-29 13:51:22 +0100

    Improved preprocessing

--- net.py
+++ net.py
@@ -1,8 +1,22 @@
 import torch
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-def train_validation_loop(trainloader, testloader, optimizer, criterion, net, epochs=1):
-    training_losses = []
+import typing
+
+def train_validation_loop(trainloader: DataLoader, valloader: DataLoader, optimizer: torch.optim.Optimizer, criterion: torch.nn.modules.loss._Loss, net, epochs: int=1, backups: bool=False):
+    """
+    Trains the model on training data while also providing current accuracy on the validation data
+
+    Parameters:
+        trainloader (DataLoader): DataLoader containing the training dataset
+        valloader (DataLoader): DataLoader containing the testing dataset
+        optimizer (Optimizer): Optimizer to be used for updating model weights
+        criterion (Loss): Loss function to calculate the loss to be minimized by the optimizer
+        net (torch.nn.Module subclass): Model to be trained
+        epochs (int): Number of training epochs
+        backups (bool): Whether backups of the model state should be made every epoch
+    """
 
     for epoch in range(epochs):
         #Training
@@ -24,8 +38,9 @@ def train_validation_loop(trainloader, testloader, optimizer, criterion, net, ep
 
             trainloop.set_description(f"Epoch [{epoch+1}/{epochs}]")
             trainloop.set_postfix(loss=training_loss/(i+1))
-        
-        training_losses.append(training_loss/len(trainloader))
+
+        if backups:
+            save_net(net, filename=net.save_file + '.epoch' + epoch)
 
         #Validation
         net.eval()
@@ -33,7 +48,7 @@ def train_validation_loop(trainloader, testloader, optimizer, criterion, net, ep
         test_loss = 0
         accuracy = 0
 
-        testloop = tqdm(testloader)
+        testloop = tqdm(valloader)
 
         for i, (images,labels_t01,labels_t02) in enumerate(testloop):
             with torch.no_grad():
@@ -41,17 +56,15 @@ def train_validation_loop(trainloader, testloader, optimizer, criterion, net, ep
                     
                 test_loss += criterion(test_predictions, labels_t01)
                                 
-                top_probability, top_guess = test_predictions.topk(1, dim=1) #Tensor of top guesses for each image
+                _, top_guess = test_predictions.topk(1, dim=1) #Tensor of top guesses for each image
                 guess_correct = top_guess == labels_t01.view(top_guess.shape) #Tensor of booleans for each guess (True: correct guess, False: wrong guess)
                 accuracy += torch.mean(guess_correct.type(torch.FloatTensor)) #Calculate mean of booleans (True:=1.0, False:=0.0)
                     
             testloop.set_description(f"> Validation on test data ")
             testloop.set_postfix(loss=test_loss.item()/(i+1),acc=accuracy.item()/(i+1))
-    
-    return training_losses
 
 
-def trainloop(trainloader, optimizer, criterion, net, epochs=1):
+def trainloop(trainloader: DataLoader, optimizer: torch.optim.Optimizer, criterion: torch.nn.modules.loss._Loss, net: torch.nn.Module, epochs: int=1) -> typing.List[int]:
     training_losses = []
 
     for epoch in range(epochs):
@@ -79,14 +92,17 @@ def trainloop(trainloader, optimizer, criterion, net, epochs=1):
     
     return training_losses
 
-def save_net(net):
-    torch.save(net.state_dict(), net.save_file)
+def save_net(net, filename: str="") -> None:
+    if filename != "":
+        torch.save(net.state_dict(), filename)
+    else:
+        torch.save(net.state_dict(), net.save_file)
 
-def load_net(net, device='cpu'):
+def load_net(net, device: str='cpu'):
     net.load_state_dict(torch.load(net.save_file, map_location=torch.device(device)))
     return net
 
-def validation_loop(testloader, criterion, net):
+def validation_loop(testloader: DataLoader, criterion: torch.nn.modules.loss._Loss, net: torch.nn.Module) -> None:
     #Validation
     net.eval()
 
@@ -101,7 +117,7 @@ def validation_loop(testloader, criterion, net):
                 
             test_loss += criterion(test_predictions, labels_t01)
                             
-            top_probability, top_guess = test_predictions.topk(1, dim=1) #Tensor of top guesses for each image
+            _, top_guess = test_predictions.topk(1, dim=1) #Tensor of top guesses for each image
             guess_correct = top_guess == labels_t01.view(top_guess.shape) #Tensor of booleans for each guess (True: correct guess, False: wrong guess)
             accuracy += torch.mean(guess_correct.type(torch.FloatTensor)) #Calculate mean of booleans (True:=1.0, False:=0.0)
                 
